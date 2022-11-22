fb24d2e30b9a226647f457f5c1ec49bb2de50662
Gwanju <gwanjuchung@yahoo.com>
2022-11-22 02:26:50 -0800

    Put in some pseudocode

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,17 @@
+// GET /
+
+// User.find()
+
+// GET /:userId
+
+// User.findById( req.params.userId )
+// .populate()
+// User.findOne( { _id: req.params.userId } )
+
+// DELETE /:userId
+
+// User.deleteOne( { _id: req.params.userId } )
+
+// PUT /:userId
+
+// User.findOneAndUpdate( search, { $set: req.body } )
