d71931dae6f0f383619800c6f9d23e205a1948b5
Gwanju <gwanjuchung@yahoo.com>
2022-11-24 01:17:10 -0800

    Added controllers for adding and removing friends

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,5 +56,34 @@ module.exports = {
             : res.json({ message: 'User successfully deleted!' })
         )
         .catch((err) => res.status(500).json(err));  
-    }
+    },
+
+    // Controllers for friends
+    addFriend(req, res) {
+        console.log('Adding a friend!');
+        User.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$addToSet: {friends: req.body}},
+            {runValidators: true, new: true}
+        )
+        .then((user) =>
+            !user 
+            ? res.status(404).json({message: 'No user with that ID'})
+            : res.json(user)
+        )
+       .catch((err) => res.status(500).json(err));
+    },    
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$pull: {friends: req.params.friendId}},
+            {runValidators: true, new: true}
+        )
+        .then((user) =>
+            !user
+            ? res.status(404).json({message: 'No user with that ID'})
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    }    
 };
