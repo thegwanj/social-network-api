d4afd2d37acc515bee98fb9e1995a546bd968480
Gwanju <gwanjuchung@yahoo.com>
2022-11-30 12:27:48 -0800

    Updating connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/social-network-api', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const url = 'mongodb://localhost:27017/socialsDB'
+
+connect(url, {useNewUrlParser: true, useUnifiedTopology: true,}, (err) => {
+  if (err) throw err;
 });
 
 module.exports = connection;
